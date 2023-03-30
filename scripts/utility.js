d243922937b94f94f7631fadd30c9500d60650ef
raknarrok <raknarrok@hotmail.com>
2023-03-30 09:22:48 -0600

    MSG Facebook Implementation Test (#14)
    Adding This to main page to review if is working on PROD

--- scripts/utility.js
+++ scripts/utility.js
@@ -45,3 +45,24 @@ $(document).ready(function () {
         }
     })
 })
+
+/**
+ * @name facebookScriptHandler
+ * @summary This method control show facebook on the page.
+ * @author Luis Fernandez
+ * @param NA
+ */
+window.fbAsyncInit = function () {
+    FB.init({
+        xfbml: true,
+        version: 'v6.0'
+    });
+};
+
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
